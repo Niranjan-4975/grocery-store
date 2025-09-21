52a301518eee2d01ebdcf1a04039e840a0cbaaf6
Niranjan-4975 <niranjandungahu@gmail.com>
2025-09-21 13:59:35 +0530

    refresh issue fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Auth from "../components/Auth/Auth.vue";
 import Home from "../components/Home/Home.vue";
 
@@ -8,7 +8,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
